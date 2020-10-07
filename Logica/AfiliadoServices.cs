8b03a57d4b27a4f51e879ae1e64af4255270001e
angeldavid2002 <angeldavidbolanoaroca@gmail.com>
2020-10-07 13:48:16 -0500

    Se confirma tercer punto

--- Logica/AfiliadoServices.cs
+++ Logica/AfiliadoServices.cs
@@ -9,5 +9,83 @@ namespace Logica
 {
     public class AfiliadoServices
     {
+        static AfiliadoRepositorio afiliadoRepositorio;
+        public AfiliadoServices()
+        {
+            afiliadoRepositorio = new AfiliadoRepositorio();
+        }
+        public string Guardar(Afiliado afiliado)
+        {
+            try
+            {
+                afiliadoRepositorio.Guardar(afiliado);
+                return "se guardo exitosamente";
+            }
+            catch (Exception e)
+            {
+
+                return "error de la aplicacion: " + (e.Message);
+            }
+        }
+        public class RespuestaAfiliado
+        {
+            public List<Afiliado> afiliaciones { get; set; }
+            public bool listaVacia { get; set; }
+            public string mensaje { get; set; }
+            public RespuestaAfiliado(List<Afiliado> afiliaciones)
+            {
+                this.afiliaciones = afiliaciones;
+                this.listaVacia = false;
+                this.mensaje = "lectura exitosa";
+            }
+            public RespuestaAfiliado(string mensaje)
+            {
+                this.listaVacia = true;
+                this.mensaje = mensaje;
+            }
+        }
+        public RespuestaAfiliado ConsultarTodos()
+        {
+            RespuestaAfiliado respuestaAfiliado;
+            try
+            {
+                if(afiliadoRepositorio.ConsultarTodos()!= null)
+                {
+                    List<Afiliado> afiliaciones = afiliadoRepositorio.ConsultarTodos();
+                    respuestaAfiliado = new RespuestaAfiliado(afiliaciones);
+                    return respuestaAfiliado;
+                }
+                else
+                {
+                    return respuestaAfiliado = new RespuestaAfiliado("no se encontraron elementos");
+                }
+            }
+            catch (Exception e)
+            {
+
+                return respuestaAfiliado = new RespuestaAfiliado("se produjo un error: "+e.Message);
+            }
+        }
+        public string AsignarNumeroLiquidacion(Afiliado afiliado)
+        {
+            try
+            {
+                if (afiliadoRepositorio.ConsultarTodos().Any())
+                {
+                    afiliado.numeroLiquidacion = afiliadoRepositorio.ConsultarTodos().Last().numeroLiquidacion + 1;
+                    return "operacion realizada con exito";
+                }
+                else
+                {
+                    afiliado.numeroLiquidacion = 1;
+                    return "operacion realizada con exito";
+                }
+            }
+            catch (Exception e)
+            {
+                return "ocurrio un error: "+e.Message;
+            }
+            
+        }
     }
 }
--- Presentacion/Program.cs
+++ Presentacion/Program.cs
@@ -10,28 +10,97 @@ namespace Presentacion
 {
     class Program
     {
+        static Afiliado afiliado;
+        static AfiliadoServices afiliadoServices = new AfiliadoServices();
         static void Main(string[] args)
         {
-            Console.WriteLine("---AFILIACION CONTRIBUTIVA---");
-            Afiliado afiliadoContributivo = new AfiliadoContributivo();
-            Console.Write("digite los dias de afiliacion: ");
-            afiliadoContributivo.diasAfiliacion = Convert.ToInt32( Console.ReadLine());
-            Console.Write("digite su sexo: ");
-            afiliadoContributivo.sexo = Convert.ToChar(Console.ReadLine());
+            int opcion;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("---MENU PRINCIPAL---");
+                Console.WriteLine("1)registrar nuevo afiliado");
+                Console.WriteLine("2)mostrar afiliados");
+                Console.WriteLine("3)mostrar liquidaciones");
+                opcion = Convert.ToInt32(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        RegistrarAfiliacion();
+                        afiliadoServices.Guardar(afiliado);
+                        break;
+                    case 2:
+                        MostrarDatos();
+                        break;
+                }
+            } while (opcion != 3);
+        }
+        static void RegistrarAfiliacion()
+        {
+            char tipoAfiliacion;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("digite su regimen: ");
+                Console.WriteLine("C) contributivo");
+                Console.WriteLine("S) subsidiado");
+                tipoAfiliacion = Convert.ToChar(Console.ReadLine().ToUpper());
+            } while ((tipoAfiliacion != 'S') && (tipoAfiliacion != 'C'));
+            if (tipoAfiliacion == 'S')
+            {
+                afiliado = new AfiliadoSubsidiado();
+            }
+            else
+            {
+                afiliado = new AfiliadoContributivo();
+            }
+            LlenarDatosAfiliacion();
+        }
+        static void LlenarDatosAfiliacion()
+        {
+            Console.Clear();
+            Console.WriteLine("---LLENAR DATOS DE CUENTA---");
+            Console.Write("digite su identificacion: ");
+            afiliado.identificacionAfiliado = Console.ReadLine();
+            Console.Write("digite su primer nombre: ");
+            afiliado.nombre1Afiliado = Console.ReadLine();
+            Console.Write("digite su segundo nombre: ");
+            afiliado.nombre2Afiliado = Console.ReadLine();
+            Console.Write("digite su primer apellido: ");
+            afiliado.apellido1Afiliado = Console.ReadLine();
+            Console.Write("digite su segundo apellido: ");
+            afiliado.apellido2Afiliado = Console.ReadLine();
+            do
+            {
+                Console.Write("digite su sexo: ");
+                afiliado.sexo = Convert.ToChar(Console.ReadLine().ToUpper());
+            } while ((afiliado.sexo != 'M') && (afiliado.sexo != 'F'));
             Console.Write("digite su edad: ");
-            afiliadoContributivo.edad = Convert.ToInt32(Console.ReadLine());
-            afiliadoContributivo.liquidacionAfiliacion = afiliadoContributivo.CalcularLiquidacionAfiliacion();
-            Console.Write(afiliadoContributivo.ImprimirDatos());
-            Console.WriteLine("---AFILIACION SUBSIDIADA---");
-            Afiliado afiliadoSubsidiado = new AfiliadoSubsidiado();
-            Console.Write("digite los dias de afiliacion: ");
-            afiliadoSubsidiado.diasAfiliacion = Convert.ToInt32(Console.ReadLine());
-            Console.Write("digite su sexo: ");
-            afiliadoSubsidiado.sexo = Convert.ToChar(Console.ReadLine());
-            Console.Write("digite su edad: ");
-            afiliadoSubsidiado.edad = Convert.ToInt32(Console.ReadLine());
-            afiliadoSubsidiado.liquidacionAfiliacion = afiliadoSubsidiado.CalcularLiquidacionAfiliacion();
-            Console.Write(afiliadoSubsidiado.ImprimirDatos());
+            afiliado.edad = Convert.ToInt32( Console.ReadLine());
+            Console.Write("digite sus dias de afiliacion: ");
+            afiliado.diasAfiliacion = Convert.ToInt32( Console.ReadLine());
+            afiliado.liquidacionAfiliacion = afiliado.CalcularLiquidacionAfiliacion();
+            Console.WriteLine(afiliadoServices.AsignarNumeroLiquidacion(afiliado));
+            Console.WriteLine("se llenaron los datos exitosamente");
+            Console.ReadKey();
+        }
+        static void MostrarDatos()
+        {
+            Console.Clear();
+            if (afiliadoServices.ConsultarTodos().listaVacia == true)
+            {
+                Console.WriteLine(afiliadoServices.ConsultarTodos().mensaje);
+            }
+            else
+            {
+                List<Afiliado> liquidaciones = afiliadoServices.ConsultarTodos().afiliaciones;
+                foreach (var item in liquidaciones)
+                {
+                    Console.WriteLine(item.ImprimirDatos());
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
